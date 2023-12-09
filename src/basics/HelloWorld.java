9748ab3a27f61c17daedc89ce650ff5f3bfda10a
mosolopg <gmosolotsane@gmail.com>
2023-12-09 23:19:28 +0200

    modified Helloworld app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,5 +3,6 @@ package basics;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("Adding code to project");
     }
 }
